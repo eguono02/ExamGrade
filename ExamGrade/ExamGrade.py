c1db3b92c7517ed259e581be5f3fc56fdd6c4490
6033119 <6033119@student.gbmc.ac.uk>
2022-01-13 14:27:18 +0000

    v2 Data Entry and Total

--- ExamGrade/ExamGrade.py
+++ ExamGrade/ExamGrade.py
@@ -2,3 +2,5 @@ print("Exam Grade Finder")
 print("-----------------")
 paper1 = int(input("Enter paper 1 mark: "))
 paper2 = int(input("Enter paper 2 mark: "))
+total = paper1 + paper2
+print("Total mark", total)
