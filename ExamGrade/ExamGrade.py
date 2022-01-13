2ca17e0372b667f2e773bc1328aa860a12f03dff
6033119 <6033119@student.gbmc.ac.uk>
2022-01-13 14:32:03 +0000

    Final version

--- ExamGrade/ExamGrade.py
+++ ExamGrade/ExamGrade.py
@@ -4,3 +4,7 @@ paper1 = int(input("Enter paper 1 mark: "))
 paper2 = int(input("Enter paper 2 mark: "))
 total = paper1 + paper2
 print("Total mark", total)
+if total >= 40:
+    print ("Pass")
+else:
+    print("Fail")
